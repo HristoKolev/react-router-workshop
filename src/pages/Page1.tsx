8f4aa2fb13af86c2a27a90c1c468033703222ec1
Hristo Kolev <hristo.a.kolev@gmail.com>
2023-01-25 01:34:08 +0200

    solution

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,8 +1,9 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useCallback } from 'react';
 
 export const Page1 = (): JSX.Element => {
   let [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   const handleOnNextClick = useCallback(() => {
     setSearchParams(
@@ -12,11 +13,18 @@ export const Page1 = (): JSX.Element => {
     );
   }, [searchParams, setSearchParams]);
 
+  const handleOnGoToPage2Click = useCallback(() => {
+    navigate('/page2/42?from=20&to=30');
+  }, [navigate]);
+
   return (
     <div>
       Page1 - {searchParams.get('from')} - {searchParams.get('to')}
       <div>
         <button onClick={handleOnNextClick}>Next</button>
+      </div>
+      <div>
+        <button onClick={handleOnGoToPage2Click}>Go to Page 2</button>
       </div>
     </div>
   );
